36d2bd0ec09ab99ca4380401d6b17f327441a540
Ulseß Simon <us4313@mci4me.at>
2026-09-24 16:24:40 +0200

    Bugfix

--- ui_plots.py
+++ ui_plots.py
@@ -21,7 +21,7 @@ class UIPlots:
         self.knoten_fontsize = knoten_fontsize
 
         #einmal die Colormap erzeugen statt wie davor in jeder plot_struktur funktion 
-        self.cmap = matplotlib.colormaps.get_cmap(self.cmap_name)
+        self.cmap = plt.get_cmap(self.cmap_name)
 
     #Interne Mehoden hier, alle mit _ gekennzeichnet 
     def _knoten_pos(self, struktur:Struktur, knoten_id: int, u=None, mapping=None, skalierung: float = 1.0):
